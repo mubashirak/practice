10fe4177073c6031942ab2e34b6923ce3e3a6708
MU257016 <MU257016@L-285002686.wipro.com>
2017-03-18 03:23:04 +0530

    first

---
+++ src/main/java/com/mubashir/practice/sop/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mubashir.practice.sop;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("just checking");
+	}
+
+}
