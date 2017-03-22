402ef121923ad7e8c6f7da4d02e5df355e57c1d5
MU257016 <MU257016@L-285002686.wipro.com>
2017-03-22 15:15:49 +0530

    Encapsulation Example 1

---
+++ src/main/java/com/mubashir/encapsulation/example1/MainEncapTest.java
@@ -0,0 +1,16 @@
+package com.mubashir.encapsulation.example1;
+
+public class MainEncapTest {
+	
+	public static void main (String [] args){
+		
+		EncapTest test = new EncapTest();
+		
+		test.setAge("12");
+		test.setName("Mu");
+		
+		System.out.println("name is " + test.getName()+" and Age is  " +test.getAge());
+	
+	}
+
+}
