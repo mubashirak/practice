c0cdceb400a63866a3b34c9fbd9ef18886f355fd
MU257016 <MU257016@L-285002686.wipro.com>
2017-03-23 20:04:31 +0530

    abstraction and encapsulation difference

---
+++ src/main/java/com/mubashir/notes/AbstractionVsEncapsulation.java
@@ -0,0 +1,11 @@
+package com.mubashir.notes;
+
+public class AbstractionVsEncapsulation {
+	
+	/**
+	 * 1. Abstraction is impleented in java using interface and abstract class while 
+	 * encapsulation is implemented using private, package protected and protected acess modifiers
+	 * 
+	 */
+
+}
