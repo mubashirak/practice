f5a702fe37758d24b6a8f2c6167a211a79edebd9
MU257016 <MU257016@L-285002686.wipro.com>
2017-04-14 21:33:02 +0530

    inheritance

---
+++ src/main/java/com/mubashir/inheritance/example1/Calculation.java
@@ -0,0 +1,18 @@
+package com.mubashir.inheritance.example1;
+
+class Calculation {
+	
+	protected int z;
+	
+	public void addition(int x , int y){
+		
+		z=x=y;
+		System.out.println("Value of  sum is" +z);
+		
+	}
+	public void Subtraction(int x, int y) {
+	      z = x - y;
+	      System.out.println("The difference between the given numbers:"+z);
+	   }
+
+}
